e90cc1f113675d8f56846e206174e32c62a1ec54
Kositthai <vkositthai@outlook.com>
2022-10-29 12:17:51 +0200

    submit evevnt application exercise

---
+++ javascript/javascript1/week2/event_application.js
@@ -0,0 +1,18 @@
+function upcomingEvent(passInDate) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date();
+  const weekday = today.getDay();
+  const eventNumber = (weekday + passInDate) % 7;
+
+  return days[eventNumber];
+}
+
+upcomingEvent(5);
