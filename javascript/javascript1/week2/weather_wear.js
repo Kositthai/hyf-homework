047c7a67bb9dddf73adc5fdef77f651eabd83fd7
Kositthai <vkositthai@outlook.com>
2022-10-30 22:25:07 +0100

    edit weatherwear to switch statement

--- javascript/javascript1/week2/weather_wear.js
+++ javascript/javascript1/week2/weather_wear.js
@@ -1,15 +1,19 @@
-function weatherWear(temperature) {
-  if (temperature < 5) {
-    console.log("winter jacket");
-  } else if (temperature <= 10) {
-    console.log("light to medium coat");
-  } else if (temperature <= 15) {
-    console.log("Fleece");
-  } else if (temperature <= 20) {
-    console.log("Short sleeves");
-  } else if (temperature <= 30) {
-    console.log("Short");
-  }
-}
+let temperature = 32; 
 
-weatherWear(15);
+switch(true){
+  case (temperature < 5): 
+  console.log('winter jacket');
+  break;
+  case (temperature <= 10):
+  console.log('light to medium coat');
+  break; 
+  case (temperature <= 15):
+  console.log('Fleece');
+  break; 
+  case (temperature <= 20):
+  console.log('Short sleeves');
+  break;
+  case (temperature <= 30 || temperature > 30 ):
+  console.log('Short');
+  break;
+}
