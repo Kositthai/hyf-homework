aaa19238ae81bc4942f15e4b8ec118e030b874db
Kositthai <vkositthai@outlook.com>
2022-11-11 10:45:02 +0100

    update calculate-percentage-series-watchin

--- javascript/javascript1/week3/calculate-percentage-series-watching.js
+++ javascript/javascript1/week3/calculate-percentage-series-watching.js
@@ -1,4 +1,3 @@
-// series duration of life //
 const seriesDurations = [
   {
     title: "Vikings",
@@ -20,17 +19,24 @@ const seriesDurations = [
   },
 ];
 
-function logOutSeriesText(nameOfSeries) {
-  let totalminutefor80Year = 42048000; // 80 * 525600
-  for (i = 0; i < seriesDurations.length; i++)
-    if (nameOfSeries === seriesDurations[i].title) {
-      let covertDayToMinute = seriesDurations[i].days * 1440;
-      let convertHoursToMinute = seriesDurations[i].hours * 60;
-      let totalMinute =
-        covertDayToMinute + convertHoursToMinute + seriesDurations[i].minutes;
-      let percentage = ((totalMinute / totalminutefor80Year) * 100).toFixed(5);
-      console.log(`${seriesDurations[i].title} took ${percentage}% of my life`);
-    }
+const totalTime = [];
+
+function logOutSeriesText() {
+  const totalminutefor80Year = 42048000; // 80 * 525600
+  let sum = 0; 
+  for (let i = 0; i < seriesDurations.length; i++) {
+    const covertDayToMinute = seriesDurations[i].days * 1440;
+    const convertHoursToMinute = seriesDurations[i].hours * 60;
+    const totalMinute = covertDayToMinute + convertHoursToMinute + seriesDurations[i].minutes;
+    const percentage = ((totalMinute / totalminutefor80Year) * 100).toFixed(5);
+
+    sum += +percentage; 
+    console.log(`${seriesDurations[i].title} took ${percentage}% of my life`);
+  
+  }
+  
+  console.log(sum.toFixed(5))
+  console.log(`In total that is ${sum.toFixed(5)} of my life`);
 }
 
-logOutSeriesText("The witcher");
+logOutSeriesText();
