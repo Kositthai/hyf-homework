7677bda95e097d7fc02f8ede248ff8a3ffdefc45
Kositthai <vkositthai@outlook.com>
2022-12-26 00:29:40 +0100

    modified variable names in html css and js

--- javascript/javascript3/week1/giphy/giphy.js
+++ javascript/javascript3/week1/giphy/giphy.js
@@ -1,46 +1,43 @@
-const input = document.querySelector('input'); 
-const inputGIFsNumber = document.getElementById('inputGIFsNumber'); 
-const searchBtn = document.getElementById('searchBtn'); 
-const displayGIFs = document.getElementById('displayGIFs'); 
+const input = document.querySelector('input');
+const inputGIFsNumber = document.getElementById('inputGIFsNumber');
+const searchBtn = document.getElementById('searchBtn');
+const displayGIFs = document.getElementById('displayGIFs');
 
-inputGIFsNumber.addEventListener('keyup', searchGIF); 
-searchBtn.addEventListener('click', searchGIF); 
+inputGIFsNumber.addEventListener('keyup', searchGIF);
+searchBtn.addEventListener('click', searchGIF);
 // assign second parameter to searchGIF same as searchBtn. the searchGIF function will run again when user typeing something in this element
 // which means first time that user search only for keyword will run the code and go to condition else if which URL link doesnt have limit attribute
 // and when the user fill in number of gifs, then searchGIF will run from the start again and this time numberOfGIFs will get value and code will run to condition if
 // which URL link will add limit attribute and take number of limit from numberOfGIFs variable.
 
-function searchGIF(){
-    
-    const keyword = input.value;  
-    const numberOfGIFs = inputGIFsNumber.value;  
-    let url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=' + API_KEY;
-    // clear content with parent element instead of child element. this will remove img tag from displayGIFs. once, its removed then the img will also disappear.
-    displayGIFs.innerHTML = ''; 
+function searchGIF() {
 
-        if(numberOfGIFs !== '' && keyword !== '') {
-                url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&limit='+ numberOfGIFs + '&api_key=' + API_KEY;
+        const keyword = input.value;
+        const numberOfGIFs = inputGIFsNumber.value;
+        let url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=' + API_KEY;
+        // clear content with parent element instead of child element. this will remove img tag from displayGIFs. once, its removed then the img will also disappear.
+        displayGIFs.innerHTML = '';
+
+        if (numberOfGIFs !== '' && keyword !== '') {
+                url = 'https://api.giphy.com/v1/gifs/search?q=' + keyword + '&limit=' + numberOfGIFs + '&api_key=' + API_KEY;
                 displayImg(url)
-        } else if (keyword !== ''){
+        } else if (keyword !== '') {
                 displayImg(url)
-        } else {alert('Please fill in keyword fo searching')}
+        } else {
+                alert('Please fill in keyword fo searching')
+        }
 }
 
 function displayImg(url) {
+        fetch(url)
+                .then(response => response.json())
+                .then(gits => {
+                        gits.data.forEach(gifs => {
+                                const imgPath = gifs.images.fixed_height.url;
+                                const img = document.createElement('img');
 
-    fetch(url)
-        .then(response => response.json())
-        .then(gits => {
-                gits.data.forEach(gifs => {                   
-                    const imgPath = gifs.images.fixed_height.url;
-                    const img = document.createElement('img'); 
-
-                    img.setAttribute('src', imgPath)                     
-                    displayGIFs.appendChild(img)
+                                img.setAttribute('src', imgPath)
+                                displayGIFs.appendChild(img)
+                        })
                 })
-        })
 }
-
-
-
-
--- javascript/javascript3/week1/weatherapp/main.js
+++ javascript/javascript3/week1/weatherapp/main.js
@@ -1,99 +1,97 @@
-const input = document.getElementById('input');
+const inputElement = document.getElementById('inputElement');
 const weatherBtn = document.getElementById('weatherBtn');
-const paraTemp = document.getElementById('paraTemp'); 
-const paraCity = document.getElementById('paraCity'); 
+const paraTemp = document.getElementById('paraTemp');
+const paraCity = document.getElementById('paraCity');
 const paraDesWeather = document.getElementById('paraDesWeather');
-const sunrise = document.getElementById('sunrise'); 
-const sunset = document.getElementById('sunset'); 
-const windSpeed = document.getElementById('windSpeed'); 
-const cloudiness = document.getElementById('cloudiness'); 
-const main = document.getElementById('main'); 
-const icon = document.getElementById('icon'); 
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const windSpeed = document.getElementById('windSpeed');
+const cloudiness = document.getElementById('cloudiness');
+const main = document.getElementById('main');
+const icon = document.getElementById('icon');
+const moreInfo = document.getElementById('moreInfo')
 
 const weatherApp = async () => {
-    const inputArea = input.value;
-    const weatherInfo = await fetchData(inputArea); 
-    const unsplash = await changeBackgroundFunc(inputArea); 
+    const inputArea = inputElement.value;
+    const weatherInfo = await fetchData(inputArea);
+    const unsplash = await changeBackgroundFunc(inputArea);
     const tempIs = Math.round(weatherInfo.main.temp - 273.15);
     const desWeather = weatherInfo.weather[0].description;
-    const srTime = new Date((weatherInfo.sys.sunrise + weatherInfo.timezone)*1000);
-    const ssTime = new Date((weatherInfo.sys.sunset + weatherInfo.timezone)*1000);
+    const srTime = new Date((weatherInfo.sys.sunrise + weatherInfo.timezone) * 1000);
+    const ssTime = new Date((weatherInfo.sys.sunset + weatherInfo.timezone) * 1000);
 
-        icon.src = 'http://openweathermap.org/img/wn/'+ weatherInfo.weather[0].icon + '@2x.png';   
+    icon.src = 'http://openweathermap.org/img/wn/' + weatherInfo.weather[0].icon + '@2x.png';
 
-        sunrise.style.background = 'rgba(107, 149, 207, 0.396)'; 
-        sunset.style.background = 'rgba(107, 149, 207, 0.396)'; 
-        windSpeed.style.background = 'rgba(107, 149, 207, 0.396)'; 
-        cloudiness.style.background = 'rgba(107, 149, 207, 0.396)'; 
-        main.style.background = 'rgba(240, 248, 255, 0.791)';
+    moreInfo.style.display = 'flex'
+    main.style.background = 'rgba(240, 248, 255, 0.791)';
 
-        paraCity.textContent = weatherInfo.name;
-        paraDesWeather.textContent = desWeather; 
+    paraCity.textContent = weatherInfo.name;
+    paraDesWeather.textContent = desWeather;
 
-        sunrise.innerHTML = 
+    sunrise.innerHTML =
         `<img src="asset/001-sunrise.png">
         <p>${getSunriseAndSunsetTime(srTime)}</p>
-        <span>Sunrise</span>`; 
+        <span>Sunrise</span>`;
 
-        sunset.innerHTML = 
+    sunset.innerHTML =
         `<img src="asset/002-sunset.png">
         <p>${getSunriseAndSunsetTime(ssTime)}</p>
-        <span>Sunset</span>`; 
+        <span>Sunset</span>`;
 
-        windSpeed.innerHTML = 
+    windSpeed.innerHTML =
         `<img src="asset/001-wind.png">
         <p>${weatherInfo.wind.speed}</p>
-        <span>Wind speed</span>`; 
+        <span>Wind speed</span>`;
 
-        cloudiness.innerHTML = 
+    cloudiness.innerHTML =
         `<img src="asset/001-clouds.png">
         <p>${weatherInfo.clouds.all}%</p>
-        <span>Cloudiness</span>`; 
+        <span>Cloudiness</span>`;
 
-        paraTemp.textContent = `${tempIs}°`;
-        document.getElementById('container').style.cssText = `background: no-repeat url(${unsplash.results[0].urls.raw}); background-size: cover;`
+    paraTemp.textContent = `${tempIs}°`;
+    document.getElementById('container').style.cssText = `background: no-repeat url(${unsplash.results[0].urls.raw}); background-size: cover;`
 
-        displayMap(weatherInfo)
+    displayMap(weatherInfo)
 }
 
 const fetchData = async (inputArea) => {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputArea}&appid=${WEATHER_API_KEY}`);
-            if(response.status === 404){
-                throw new Error('something went wrong')
-            }            
-            const weatherInfo = await response.json()
-            return weatherInfo;  
+        if (response.status === 404) {
+            throw new Error('something went wrong')
+        }
+        const weatherInfo = await response.json()
+        return weatherInfo;
 
-    } catch(error) {
-            alert('Sorry, we cannot find the location you seached for...'); 
-            console.log(error.message)
-            return error.message; 
-    }    
-            
+    } catch (error) {
+        alert('Sorry, we cannot find the location you seached for...');
+        console.log(error.message)
+        return error.message;
+    }
+
 }
 
-const changeBackgroundFunc = async(inputArea) => {
+const changeBackgroundFunc = async (inputArea) => {
     try {
         const response = await fetch(`https://api.unsplash.com/search/photos?query=${inputArea}&orientation=portrait&client_id=${UNSPLASH_API_KEY}`);
 
-        if(response.status !== 200){
+        if (response.status !== 200) {
             throw new Error('something went wrong')
         }
-            const unsplash = response.json()
-            return unsplash; 
-           
-    } catch(error) {
+        const unsplash = response.json()
+        return unsplash;
+
+    } catch (error) {
         console.log(error.message)
-        return error.message; 
+        return error.message;
     }
 }
 
-function getSunriseAndSunsetTime(sunTime){     
-    const twoDigits = (val) => ('0' + val).slice(-2); 
+function getSunriseAndSunsetTime(sunTime) {
+    const twoDigits = (val) => ('0' + val).slice(-2);
     const hours = twoDigits(sunTime.getUTCHours());
     const minutes = twoDigits(sunTime.getUTCMinutes());
-        
+
     return (`${hours}:${minutes}`);
 }
 
@@ -101,21 +99,21 @@ function displayMap(weatherInfo) {
     const mapid = document.getElementById('mapid')
     const mapSection = document.getElementById('mapSection')
     mapid.remove();
-    
+
     mapSection.innerHTML = `
     <div id="mapWrapper" style="background: rgba(107, 149, 207, 0.396)">
     <p id="location"><i class="fas fa-map-marker-alt"></i>  Location</p>
         <div id="mapid"></div>
     </div>
-    `; 
-    
+    `;
+
     const map = L.map('mapid').setView([weatherInfo.coord.lat, weatherInfo.coord.lon], 8);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        
-        L.marker([weatherInfo.coord.lat, weatherInfo.coord.lon]).addTo(map);    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([weatherInfo.coord.lat, weatherInfo.coord.lon]).addTo(map);
 }
 
-weatherBtn.addEventListener('click', weatherApp); 
+weatherBtn.addEventListener('click', weatherApp);
