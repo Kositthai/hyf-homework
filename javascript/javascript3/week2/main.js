e6d2723417404c7fc0a9ab846128642891c8f159
Kositthai <vkositthai@outlook.com>
2023-01-23 13:07:03 +0100

    fixed fetch then catch

--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -1,90 +1,98 @@
-const inputEl = document.getElementById('inputEl'); 
-const convertFrom = document.getElementById('convertFrom'); 
-const convertTo = document.getElementById('convertTo'); 
-const btn = document.getElementById('btn');
-const displayCurrency = document.getElementById('displayCurrency');
+const convertFrom = document.getElementById("convert-from");
+const convertTo = document.getElementById("convert-to");
+const convertBtn = document.getElementById("convert-btn");
 
-// display option accroding to currency in rates
-const createOption = async () => {
-    try {
-        const response = await fetch('https://open.er-api.com/v6/latest/USD')
-            if(response.status !== 200 ){
-                throw new Error('something went wrong')
-            }
-                const data = await response.json()
-                const rates = data.rates; 
-                const keys = Object.keys(rates); 
-                createOptions(keys) 
-            } catch(error){
-                alert(`Sorry, there was a problem with the operation.`)
-                console.log(error.message)
-                return error.message; 
-            }
-}
+const createOption = () => {
+  fetch(`https://open.er-api.com/v6/latest/USD`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const rates = data.rates;
+      const keys = Object.keys(rates);
+      createOptions(keys);
+    })
+    .catch((error) => {
+      alert(`Sorry, there was a problem with the operation.`);
+      console.log(error.message);
+      return error.message;
+    });
+};
 
-createOption()
+createOption();
 
 const createOptions = (keys) => {
-    keys.forEach(options => {
-        // create opt1 & opt2 to not make it overwritten each other 
-        // because if we createElement only one time, the convertTo element will overwritten convertFrom.
-        const opt1 = document.createElement('option'); 
-        opt1.textContent = options; 
-        opt1.setAttribute('value', options);
-        convertFrom.appendChild(opt1); 
-        
-        const opt2 = document.createElement('option'); 
-        opt2.textContent = options; 
-        opt2.setAttribute('value', options); 
-        convertTo.appendChild(opt2)
-    })
+  keys.forEach((options) => {
+    // create opt1 & opt2 to not make it overwritten each other
+    // because if we createElement only one time, the convertTo element will overwritten convertFrom.
+    const opt1 = document.createElement("option");
+    opt1.textContent = options;
+    opt1.setAttribute("value", options);
+    convertFrom.appendChild(opt1);
 
-    const optionOne = convertFrom.querySelectorAll('option')
-        for(options of optionOne){
-            if(options.value === 'EUR'){
-                    options.selected = true;
-            }
-        }
+    const opt2 = document.createElement("option");
+    opt2.textContent = options;
+    opt2.setAttribute("value", options);
+    convertTo.appendChild(opt2);
+  });
 
-    const optionTwo = convertTo.querySelectorAll('option')
-        for(options of optionTwo){
-            if(options.value === 'DKK'){
-                    options.selected = true;
-            }
-        }
-}
-
-const fetchFunc = async (search) => {   
-    try {
-        const response = await fetch(`https://open.er-api.com/v6/latest/${search}`)
-            if(response.status !== 200){
-               throw new Error('something went wrong'); 
-            } 
-                const data = await response.json()
-                return data; 
-    } catch(error){
-        alert(`Sorry, there was a problem with the operation. ${error.message}`)
-        console.log(error.message)
-        return error.message; 
-    }   
-}
-
-const currencyApp = async() => {
-    const inputValue = inputEl.value;
-    const convertToValue = convertTo.value; 
-    const convertFromValue = convertFrom.value; 
-    const fetchData = await fetchFunc(convertFromValue); 
-
-    if(inputValue > 0){
-    const rateSelector = fetchData.rates[convertToValue]; // because of convertToValue is a variable so we need to use braceket notation in this case. 
-                const amount = rateSelector * inputValue; 
-                
-                displayCurrency.innerHTML = 
-                    `<p>${inputValue} ${convertFromValue} =</p>
-                    <h2>${amount} ${convertToValue}</h2>`;
-    } else {
-        alert('Please insert amount to convert')
+  const optionOne = convertFrom.querySelectorAll("option");
+  for (options of optionOne) {
+    if (options.value === "EUR") {
+      options.selected = true;
     }
-}
+  }
 
-btn.addEventListener('click', currencyApp)
+  const optionTwo = convertTo.querySelectorAll("option");
+  for (options of optionTwo) {
+    if (options.value === "DKK") {
+      options.selected = true;
+    }
+  }
+};
+
+const fetchFunc = async (search) => {
+  return fetch(`https://open.er-api.com/v6/latest/${search}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("something went wrong");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      alert(`Sorry, there was a problem with the operation. ${error.message}`);
+      console.log(error.message);
+      return error.message;
+    });
+};
+
+const currencyApp = async () => {
+  const amountInput = document.getElementById("amount-input");
+  const amountValue = amountInput.value;
+  const convertToValue = convertTo.value;
+  const convertFromValue = convertFrom.value;
+  const fetchData = await fetchFunc(convertFromValue);
+  const displayCurrency = document.getElementById("display-currency");
+
+  if (!isNaN(amountValue)) {
+    if (amountValue > 0) {
+      const rateSelector = fetchData.rates[convertToValue]; // because of convertToValue is a variable so we need to use braceket notation in this case.
+      const amount = rateSelector * amountValue;
+      displayCurrency.innerHTML = 
+      `<p>${parseInt(amountValue).toLocaleString("en-US")} ${convertFromValue}</p>
+       <h2>${amount.toLocaleString("en-US")} ${convertToValue}</h2>`;
+    } else {
+      alert("Please insert amount to convert");
+    }
+  } else {
+    alert("This input can only contain numbers");
+  }
+};
+
+convertBtn.addEventListener("click", currencyApp);
