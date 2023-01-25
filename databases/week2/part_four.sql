1eb0ccf886ff70a6e0815811d97b813dab887290
Kositthai <vkositthai@outlook.com>
2023-01-25 21:40:28 +0100

    I redesigned the whole database and also insert data for testing

--- databases/week2/part_four.sql
+++ databases/week2/part_four.sql
@@ -4,54 +4,60 @@ CREATE DATABASE IF NOT EXISTS `hw_lesson2_part4`;
 
 USE `hw_lesson2_part4`;
 
-CREATE TABLE employee_details( 
-	id INT NOT NULL PRIMARY KEY, 
-    salary INT NOT NULL,
-    department_id INT UNIQUE NOT NULL, 
-    start_date DATE NOT NULL,
-    end_date DATE NULL,
-    status_id INT UNIQUE NOT NULL,
-    INDEX(start_date, end_date) 
-)ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE=UTF8MB4_UNICODE_CI;
+CREATE TABLE customer (
+	id INT(10) NOT NULL PRIMARY KEY, 
+    name VARCHAR(35) NOT NULL, 
+    email VARCHAR(35) NOT NULL UNIQUE, 
+    phone VARCHAR(20) NULL
+);
 
-CREATE TABLE employees( 
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR(255) NOT NULL, 
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone VARCHAR(255) NULL, 
-    CONSTRAINT fk_emp_id FOREIGN KEY(id) REFERENCES employee_details(id),
-    INDEX(name)
-)ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE=UTF8MB4_UNICODE_CI;
+CREATE TABLE type_of_room (
+	id INT(10) NOT NULL UNIQUE PRIMARY KEY, 
+    name VARCHAR(35) NOT NULL UNIQUE
+);
 
-CREATE TABLE employee_status( 
-	status_id INT NOT NULL PRIMARY KEY, 
-    name VARCHAR(255) NOT NULL, 
-    CONSTRAINT fk_emp_status_id FOREIGN KEY(status_id) REFERENCES employee_details(status_id)
-)ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE=UTF8MB4_UNICODE_CI; 
+CREATE TABLE payment (
+	id INT(10) NOT NULL PRIMARY KEY, 
+    payment_method VARCHAR(35) NOT NULL UNIQUE
+);
 
-CREATE TABLE shops( -- parent table 
-	shop_id INT NOT NULL PRIMARY KEY, 
-    name VARCHAR(255) NOT NULL UNIQUE, 
-    phone VARCHAR(255) NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+CREATE TABLE booking (
+	id INT(10) NOT NULL PRIMARY KEY, 
+    customer_id INT(10) NOT NULL,
+	type_of_room_id INT(10) NOT NULL, 
+    payment_id INT(10) NOT NULL,
+    checkin_date DATE NOT NULL, 
+	checkout_date DATE NULL, 
+    CONSTRAINT fk_cutomer_id FOREIGN KEY(customer_id) REFERENCES customer(id), 
+    CONSTRAINT fk_payment FOREIGN KEY(payment_id) REFERENCES payment(id)
+); 
 
-CREATE TABLE employee_shops (
-	shop_id INT NOT NULL AUTO_INCREMENT, 
-	id INT NOT NULL, 
-    PRIMARY KEY(id, shop_id), 
-    CONSTRAINT fk_emp_id_for_employee_shops FOREIGN KEY(id) REFERENCES employees(id), 
-    CONSTRAINT fk_shop_id FOREIGN KEY(shop_id) REFERENCES shops(shop_id) 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+CREATE TABLE booking_room(
+	booking_id INT(10), 
+    type_of_room_id INT(10), 
+    PRIMARY KEY(booking_id, type_of_room_id),
+    CONSTRAINT fk_booking_id FOREIGN KEY(booking_id) REFERENCES booking(id), 
+	CONSTRAINT fk_type_of_room_id FOREIGN KEY(type_of_room_id) REFERENCES type_of_room(id)
+);
 
-CREATE TABLE departments(
-	department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    department_name VARCHAR(255) NOT NULL, 
-    title_id INT UNIQUE NOT NULL, 
-    CONSTRAINT fk_department_id FOREIGN KEY(department_id) REFERENCES employee_details(department_id) 
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO customer (id, name, email, phone) VALUES (1, 'John', 'john@test.com', NULL);
+INSERT INTO customer (id, name, email, phone) VALUES (2, 'Adam', 'adam@test.com', '42-52-63-85');
+INSERT INTO customer (id, name, email, phone) VALUES (3, 'Marie', 'marie@test.com', '01-78-52-33');
+INSERT INTO customer (id, name, email, phone) VALUES (4, 'Mads', 'mads@test.com', '85-32-41-65');
+INSERT INTO customer (id, name, email, phone) VALUES (5, 'Sofie', 'sofie@test.com', NULL);
 
-CREATE TABLE job_titles(
-	title_id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    title_name VARCHAR(255), 
-    CONSTRAINT fk_title_id FOREIGN KEY(title_id) REFERENCES departments(title_id)
-)ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE=UTF8MB4_UNICODE_CI;
+INSERT INTO payment(id, payment_method) VALUES (1, 'Pay at hotel'); 
+INSERT INTO payment(id, payment_method) VALUES (2, 'Credit card'); 
+INSERT INTO payment(id, payment_method) VALUES (3, 'Debit card');
+
+INSERT INTO type_of_room (id, name) VALUES(1, 'Standard room'); 
+INSERT INTO type_of_room (id, name) VALUES(2, 'Deluxe room'); 
+INSERT INTO type_of_room (id, name) VALUES(3, 'Suit room'); 
+
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (1, 1, 2, 2, '2023-01-23', '2023-01-26');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (2, 1, 1, 2, '2023-01-24', '2023-01-26');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (3, 2, 2, 1, '2023-01-20', '2023-01-22');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (4, 3, 3, 2, '2023-01-22', '2023-01-23');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (5, 4, 1, 3, '2023-01-30', '2023-01-31');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (6, 5, 2, 3, '2023-01-19', '2023-01-24');
+INSERT INTO booking (id, customer_id, type_of_room_id, payment_id, checkin_date, checkout_date) VALUES (7, 2, 3, 2, '2023-02-15', '2023-01-19');
