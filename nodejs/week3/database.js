81aa0103a3ddc7775c1cd0964dd408f78b75be00
Kositthai <vkositthai@outlook.com>
2023-02-17 11:31:54 +0100

    add database

---
+++ nodejs/week3/database.js
@@ -0,0 +1,12 @@
+const knex = require("knex")({
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSW,
+      multipleStatements: true,
+    },
+  });
+
+module.exports = knex;  
