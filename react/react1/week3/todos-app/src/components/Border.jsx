ffd252f25af1e814f7d6b706134675cf75be6ee6
Kositthai <vkositthai@outlook.com>
2023-03-15 20:39:28 +0100

    Revert "added more funtionality for todo-app add, delete, update"
    This reverts commit 51331c51d38f9db306cf6c77a29cddfb9347be47.

--- react/react1/week3/todos-app/src/components/Border.jsx
+++
@@ -1,7 +0,0 @@
-export default function Border(props) {
-    return ( 
-        <div style={{border: "solid", margin: "1em", padding:"1em"}}>
-            {props.children}
-        </div>
-     );
-}
--- react/react1/week3/todos-app/src/components/ToDoHeader.jsx
+++
@@ -1,3 +0,0 @@
-export default function ToDoHeader({ title }) {
-  return <h1>{title}</h1>;
-}
