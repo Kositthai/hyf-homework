a9246af6690791b9bebccc279a505fa749a468b8
Kositthai <vkositthai@outlook.com>
2023-04-07 13:04:43 +0200

    add renderUser function to UserSearcher component instead of SearchContext

--- react/react2/week_1/github-user-searcher/src/Component/UserSearcher.jsx
+++ react/react2/week_1/github-user-searcher/src/Component/UserSearcher.jsx
@@ -1,19 +1,29 @@
 import { SearchingContext } from "../SearchContext";
-import { useContext } from "react"
+import { useContext } from "react";
 
 export default function UserSeacher() {
-  const { handleInputChange, query, userList, loading, renderUser, error } = useContext(SearchingContext);
+  const { query, setQuery, userList, loading, error } = useContext(SearchingContext);
+ 
+  const renderUser = () => {
+    return userList?.map((user) => <p key={user.id}>{user.login}</p>);
+  };
 
   return (
     <div>
       <div>
         <h1>Github user seacher</h1>
-        <input type="text" onChange={handleInputChange} />
+        <input
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
       </div>
       <div>
         {loading ? <h1>Loading...</h1> : null}
         {query.length && userList ? renderUser() : <p>No Item</p>}
-        {error && error !== "The user aborted a request." ? <p>Error Fetching: {error}</p> : null}
+        {error && error !== "The user aborted a request." ? (
+          <p>Error Fetching: {error}</p>
+        ) : null}
       </div>
     </div>
   );
